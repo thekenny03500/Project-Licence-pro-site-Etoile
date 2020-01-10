3e04d0af912e8601af6c926468c28eded1b564b8
thekenny03500 <dalvai.aimeric@gmail.com>
2020-01-10 15:30:34 +0100

    Add_Test_getInfo

--- packages/http-client/lib/client.js
+++ packages/http-client/lib/client.js
@@ -2,7 +2,7 @@ module.exports = function(host, port) {
     
     function getAll() {
         return new Promise((resolve, reject) => {
-            fetch(`http://${host}:${port}/api/change`)
+            fetch(`http://${host}:${port}/api/stars`)
                 .then((response) => {
                     resolve(response.json());
                 })
@@ -11,7 +11,7 @@ module.exports = function(host, port) {
     }
     function add() {
         return new Promise((resolve, reject) => {
-            fetch('http://localhost:7890/api/change', {
+            fetch('http://localhost:7890/api/stars', {
             method: 'post',
             body: JSON.stringify({eee: "uuuuu"})
         }).then(resolve).catch(reject)
--- packages/http-client/lib/tests/index.js
+++ packages/http-client/lib/tests/index.js
@@ -15,18 +15,29 @@ describe('Test API', () => {
     afterEach(fetchMock.resetBehavior)
 
     describe('Test Get', () => {
-        it('test /', async () => {
-            fetchMock.get(`http://localhost:7890/api/change`, [{attribute_a: 'AAA', attribute_b: 'BBB', attribute_c: 333}]);
+
+        const listeStar = [{"id":"cd5b95b2-fb42-4b23-b8e7-6dd8a03e19ae","name":"All Passion Spent","galaxy":"Little Hands Clapping","distance":null},{"id":"b0a40133-3c80-4c39-aeac-015d8710c603","name":"Tiger! Tiger!","galaxy":"An Evil Cradling","distance":null}];
+
+        it('test / getAll', async () => {
+            fetchMock.get(`http://localhost:7890/api/stars`,listeStar );
             const client = httpclient('localhost', 7890);
             client.getAll().then((changes) => {
-                expect(changes).to.eql([{attribute_a: 'AAA', attribute_b: 'BBB', attribute_c: 333}]);
+                expect(changes).to.eql(listeStar);
             })
+        });
+        it('test / getInfo', async () => {
+            const star = listeStar[0];
+            fetchMock.get(`http://localhost:7890/api/stars/`+star.id,star );
+            const client = httpclient('localhost', 7890);
+            client.getInfo(star.id).then((changes)=>{
+                expect(changes).to.eql(star);
+            });
         });
     });
 
     describe('Test Post', () => {
         it('test /', async () => {
-            fetchMock.post(`http://localhost:7890/api/change`, {eeee: "tttt"});
+            fetchMock.post(`http://localhost:7890/api/stars`, {eeee: "tttt"});
             const client = httpclient('localhost', 7890);
             client.add().then((changes) => {
                 const request = fetchMock.lastCall()[1];
