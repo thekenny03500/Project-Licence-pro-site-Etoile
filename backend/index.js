62c0ff3bc8e699b9ad6bc262088bd33b57cc77c4
thekenny03500 <dalvai.aimeric@gmail.com>
2020-01-23 15:27:02 +0100

    Add func Post /api/stars and modif func Get /api/stars and /api/stars/:id

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,48 @@
 const express = require('express');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const uuidv1 = require('uuid/v1');
 
 const { Pool } = require('pg');
 const validator = require('validator');
 
 const app = express();
+const poolLogin = {
+            user: 'db_user',
+            host: 'database',
+            database: 'db_db',
+            password: 'db_password'
+          };
 
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// getAll Stars
+// ************ //
+// getAll Stars //
+// ************ //
+// Get all stars in database and return them
 app.get('/api/stars', (req, res) => {
-    const pool = new Pool({
-        user: 'db_user',
-        host: 'database',
-        database: 'db_db',
-        password: 'db_password'
-      });
+    const pool = new Pool(poolLogin);
 
-    pool.query('SELECT id,name,galaxy,distance FROM stars;', (err, result) => {
-        pool.end();
-        res.status(200).send(JSON.stringify(result.rows));
-    });
+    const query = {
+        text: 'SELECT id,name,galaxy,distance FROM stars;'
+    }
+
+    pool.query(query)
+        .then(result => {
+            pool.end();
+            res.status(200).send(JSON.stringify(result.rows));
+        }).catch(err => {
+                console.error(err.stack);
+                res.status(500);
+        });
 });
 
-// getInfo Stars
+// ************* //
+// getInfo Stars //
+// ************* //
+// Get star by UUID in database and return it
 app.get('/api/stars/:id', (req, res) => {
 
     if(!validator.isUUID(req.params.id))
@@ -32,48 +51,62 @@ app.get('/api/stars/:id', (req, res) => {
     }
     else
     {
-        const pool = new Pool({
-            user: 'db_user',
-            host: 'database',
-            database: 'db_db',
-            password: 'db_password'
-          });
+        const pool = new Pool(poolLogin);
 
-        pool.query('SELECT id,name,galaxy,distance FROM stars WHERE id=\''+req.params.id+'\';', (err, result) => {
-            pool.end();
-            if(result.rows.length <1)
-                res.status(404).send('Not found');
-            else
-                res.status(200).send(JSON.stringify(result.rows[0]));
-        });
+        const query = {
+            text: 'SELECT id,name,galaxy,distance FROM stars WHERE id=$1;',
+            values: [req.params.id],
+        }
+
+        pool.query(query)
+            .then(result => {
+                pool.end();
+                if(result.rows.length <1)
+                    res.status(404).send('Not found');
+                else
+                    res.status(200).send(JSON.stringify(result.rows[0]));
+            }).catch(err => {
+                console.error(err.stack);
+                res.status(500);
+            })
     }
 });
 
-// addStar Post
-/*
+// ************ //
+// addStar Post //
+// ************ //
+// Add a new star in database and return it with the UUID
 app.post('/api/stars', (req, res) => {
-
-    if()
+    let newStar = req.body;
+    if(newStar
+    && newStar.name
+    && newStar.galaxy
+    && newStar.distance)
     {
-        res.status(400).send('UUID Incorrect');
+        const pool = new Pool(poolLogin);
+        const uuid = uuidv1();
+        newStar.id = uuid;
+
+        const query = {
+            text: 'INSERT INTO stars(id,name,galaxy,distance) VALUES($1, $2, $3, $4);',
+            values: [newStar.id, newStar.name, newStar.galaxy, newStar.distance],
+        }
+
+        pool.query(query)
+            .then(result => {
+                pool.end();
+                res.status(201).send(JSON.stringify(newStar));
+            }).catch(err => {
+                console.error(err.stack);
+                res.status(500);
+            });
     }
     else
     {
-        const pool = new Pool({
-            user: 'db_user',
-            host: 'database',
-            database: 'db_db',
-            password: 'db_password'
-          });
-
-        pool.query('SELECT id,name,galaxy,distance FROM stars WHERE id=\''+req.params.id+'\';', (err, result) => {
-            pool.end();
-            if(result.rows.length <1)
-                res.status(404).send('Not found');
-            else
-                res.status(200).send(JSON.stringify(result.rows[0]));
-        });
+        res.status(400).send("invalid request");
     }
-});*/
+});
+
+
 
 module.exports = app;
